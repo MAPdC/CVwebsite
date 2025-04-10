accfd37dd7f7e67906b1617a2079e6fc5255e1ed
Miguel Ângelo Pinto da Cunha <mpintodacunha@gmail.com>
2025-04-10 15:05:33 +0100

    add products

---
+++ src/mocks/products.js
@@ -0,0 +1,23 @@
+// mocks/products.js
+export const wines = [
+    { 
+      id: 1, 
+      name: "Casttêdo Reserva", 
+      year: 2020,
+      description: "...",
+      price: 24.99
+      // outras propriedades
+    },
+    // outros vinhos
+  ];
+  
+  export const oliveOils = [
+    {
+      id: 1,
+      name: "Azeite Virgem Extra Casttêdo",
+      description: "...",
+      price: 15.99
+      // outras propriedades
+    },
+    // outros azeites
+  ];
